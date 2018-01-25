0ac738c2860e9b6aa10d67b60eb1f6fb5c94ef51
Sophie Koonin <sophie.koonin@gmail.com>
2018-01-25 17:10:26 +0000

    Add redux and store Remove yarn file as it was interfering with NPM. Update package.json

---
+++ src/store.js
@@ -0,0 +1,39 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import { routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import createSagaMiddleware from 'redux-saga'
+import { createLogger } from 'redux-logger'
+
+export const history = createHistory()
+const loggerMiddleware = createLogger()
+
+const sagaMiddleware = createSagaMiddleware()
+const initialState = {}
+const enhancers = []
+
+const middleware = [
+  routerMiddleware(history),
+  sagaMiddleware,
+  loggerMiddleware
+]
+
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.devToolsExtension
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension())
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+)
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composedEnhancers
+)
+
+export default store

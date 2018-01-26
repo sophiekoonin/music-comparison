a2fb04b141fc9af86e7c700b4c11b4c13317d45b
Sophie Koonin <sophie.koonin@gmail.com>
2018-01-26 13:38:37 +0000

    new files + package updates

---
+++ test/services/Spotify.test.js
@@ -0,0 +1,10 @@
+import '../testGlobals'
+
+describe('Spotify service', () => {
+  context('authorization', () => {
+    it('should send a request to the Spotify auth endpoint', () => {
+
+      }
+    )
+  })
+})
---
+++ test/testGlobals.js
@@ -0,0 +1,5 @@
+import chai from 'chai'
+
+beforeEach(() => {
+  chai.use(require('chai-enzyme')())
+})
